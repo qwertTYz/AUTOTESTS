e2ae25837a1d042a695722b449ff1576374c8323
qwertTYz <daniil.prib@gmail.com>
2025-05-01 15:02:43 +0300

    init

---
+++ autotest task 6 approaches/UnitTest1.cs
@@ -0,0 +1,16 @@
+namespace autotest_task_6_approaches
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
